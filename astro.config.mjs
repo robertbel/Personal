a144ca7df7fcb63cb72e44286c7ea8d613f6d51b
robertbel <r.bellekom@gmail.com>
2024-12-12 16:40:52 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    functionPerRoute: false,
-    runtime: 'nodejs18.x'
-  }),
+  output: 'static',
+  adapter: vercel(),
 });
